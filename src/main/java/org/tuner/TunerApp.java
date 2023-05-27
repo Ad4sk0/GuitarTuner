2d7f963d65f07e4f9756eeeaed74d799c21e5226
Ad4sk0 <>
2023-05-27 09:57:00 +0200

    Add logging configuration

--- src/main/java/org/tuner/TunerApp.java
+++ src/main/java/org/tuner/TunerApp.java
@@ -5,8 +5,11 @@ import javafx.stage.Stage;
 import org.tuner.detector.observer.DetectionProducer;
 import org.tuner.frontend.MainWindow;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.util.concurrent.Executors;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class TunerApp extends Application {
@@ -15,7 +18,21 @@ public class TunerApp extends Application {
     private GuitarTuner guitarTuner;
 
     public static void main(String[] args) {
+        configureLogging();
         launch();
+    }
+
+    private static void configureLogging() {
+        String loggingPropertiesFileName = "logging.properties";
+        URL loggingProperties = TunerApp.class.getClassLoader().getResource(loggingPropertiesFileName);
+        if (loggingProperties == null) {
+            throw new IllegalStateException(String.format("Unable to find logging properties file: %s", loggingPropertiesFileName));
+        }
+        try (var fileInputStream = new FileInputStream(loggingProperties.getFile())) {
+            LogManager.getLogManager().readConfiguration(fileInputStream);
+        } catch (IOException e) {
+            throw new IllegalStateException("Unable to setup logger: " + e);
+        }
     }
 
     @Override
